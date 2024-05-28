b3aafeb6ca44379b139889493ddddda35163c003
Devin Hunter <devhunter16@gmail.com>
2024-05-28 09:15:02 -0700

    Fixing some more pathing and adding a FlatList element.

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,12 +1,15 @@
-import { View } from 'react-native';
+import { View, FlatList } from 'react-native';
 import ProductListItem from '@components/ProductListItem';
 import products from '@assets/data/products';
 
+// We're using a FlatList in this function. A FlatList enables infinite scroll vertically
+// or horizontally. FlatLists hve two required properties: the data (a list) and renderItem.
+// renderItem should be a function that tells FlatList how should one single item from the
+// array we provided to the data property be rendered.
 export default function MenuScreen() {
   return (
     <View>
-      <ProductListItem product={products[0]} />
-      <ProductListItem product={products[1]} />
+      <FlatList data={products} renderItem={() => <ProductListItem product={products[0]} />} />
     </View>
   );
 };
