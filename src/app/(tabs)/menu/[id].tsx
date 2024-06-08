8f888eb504c1489ff261628b3b31a631e4ed6624
Devin Hunter <devhunter16@gmail.com>
2024-06-08 12:28:16 -0700

    Changing some header titles.

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,4 +1,4 @@
-import { useLocalSearchParams } from 'expo-router';
+import { Stack, useLocalSearchParams } from 'expo-router';
 import { View, Text } from 'react-native';
 
 export default function ProductDetailsScreen() {
@@ -7,6 +7,7 @@ export default function ProductDetailsScreen() {
 
     return (
         <View>
+            <Stack.Screen options={{ title: `Details: ${id}` }} />
             <Text>ProductDetailsScreen for id: {`${id}`}</Text>
         </View>
     );
--- src/app/(tabs)/menu/_layout.tsx
+++ src/app/(tabs)/menu/_layout.tsx
@@ -2,6 +2,14 @@ import { Stack } from 'expo-router';
 
 // A _layout.tsx component file wraps around the child components in the same folder.
 // It is also a good place to use data or context providers etc.
+
+// Stack.Screen here targets our index file (name="index") in the same folder and
+// changes the header title to "Menu" rather than just "index" so we see that iin
+// our app.
 export default function MenuStack() {
-    return <Stack />
+    return (
+        <Stack>
+            <Stack.Screen name="index" options={{ title: 'Menu' }} />
+        </Stack>
+    );
 };
