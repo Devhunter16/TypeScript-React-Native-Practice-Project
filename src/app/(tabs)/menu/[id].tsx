6d4cf412851e66a70b7877b42e7be894bb2fdbb1
Devin Hunter <devhunter16@gmail.com>
2024-06-09 09:22:07 -0700

    Adding extra comments for future reference.

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -2,7 +2,8 @@ import { Stack, useLocalSearchParams } from 'expo-router';
 import { View, Text } from 'react-native';
 
 export default function ProductDetailsScreen() {
-    // useLocalSearchParams is a hook imported from expo-router.
+    // useLocalSearchParams is a hook imported from expo-router that is used to extract
+    // dymaic data passed via the URL to a spefici screen or component.
     const { id } = useLocalSearchParams();
 
     return (
