1c7bc8b4fc1340f3253043632f1fdc6b9974a5a8
Devin Hunter <devhunter16@gmail.com>
2024-06-14 14:04:11 -0700

    Creating the Product Details Screen with appropriate styling.

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,15 +1,42 @@
 import { Stack, useLocalSearchParams } from 'expo-router';
-import { View, Text } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
+import products from 'assets/data/products';
+import defaultPizzaImage from '@components/ProductListItem';
 
 export default function ProductDetailsScreen() {
     // useLocalSearchParams is a hook imported from expo-router that is used to extract
     // dymaic data passed via the URL to a spefici screen or component.
     const { id } = useLocalSearchParams();
 
+    const product = products.find((item) => item.id.toString() === id);
+
+    // If product is undefined return this.
+    if (!product) {
+        return <Text>Product not found</Text>;
+    };
+
+    // If product is found return this.
     return (
-        <View>
-            <Stack.Screen options={{ title: `Details: ${id}` }} />
-            <Text>ProductDetailsScreen for id: {`${id}`}</Text>
+        <View style={styles.container}>
+            <Stack.Screen options={{ title: product.name }} />
+            <Image source={{ uri: product.image || defaultPizzaImage }} style={styles.image} />
+            <Text style={styles.price}>${product.price}</Text>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'white',
+        flex: 1,
+        padding: 10
+    },
+    image: {
+        width: '100%',
+        aspectRatio: 1
+    },
+    price: {
+        fontSize: 18,
+        fontWeight: 'bold'
+    }
+});
