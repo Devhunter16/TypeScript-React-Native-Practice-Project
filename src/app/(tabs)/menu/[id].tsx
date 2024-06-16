86fb018ebbcd7a2451ab38f11c27f653ca06c039
Devin Hunter <devhunter16@gmail.com>
2024-06-16 09:10:39 -0700

    Added size selector to the product details screen.

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -3,6 +3,8 @@ import { View, Text, Image, StyleSheet } from 'react-native';
 import products from 'assets/data/products';
 import { defaultPizzaImage } from '@components/ProductListItem';
 
+const sizes = ['S', 'M', 'L', 'XL'];
+
 export default function ProductDetailsScreen() {
     // useLocalSearchParams is a hook imported from expo-router that is used to extract
     // dymaic data passed via the URL to a spefici screen or component.
@@ -24,6 +26,14 @@ export default function ProductDetailsScreen() {
         <View style={styles.container}>
             <Stack.Screen options={{ title: product.name }} />
             <Image source={{ uri: product.image || defaultPizzaImage }} style={styles.image} />
+            <Text>Select size</Text>
+            <View style={styles.sizes}>
+                {sizes.map(size => (
+                    <View key={size} style={styles.size}>
+                        <Text style={styles.sizeText}>{size}</Text>
+                    </View>
+                ))}
+            </View>
             <Text style={styles.price}>${product.price}</Text>
         </View>
     );
@@ -42,5 +52,22 @@ const styles = StyleSheet.create({
     price: {
         fontSize: 18,
         fontWeight: 'bold'
+    },
+    sizes: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        marginVertical: 10
+    },
+    size: {
+        backgroundColor: 'gainsboro',
+        width: 50,
+        aspectRatio: 1,
+        borderRadius: 25,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    sizeText: {
+        fontSize: 20,
+        fontWeight: '500'
     }
 });
