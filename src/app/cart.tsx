943f9317418f5881f33ebae0a8aca3e9887abf87
Devin Hunter <devhunter16@gmail.com>
2024-06-26 16:55:33 -0700

    Adding <StatusBar styling on ios to show the battery, wifi, etc on the phone above our app.

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,9 +1,13 @@
-import { View, Text } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { Platform, View, Text } from 'react-native';
 
 const CartScreen = () => {
     return (
         <View>
             <Text>Cart</Text>
+
+            {/* Use a light status bar on iOS to account for the black space above the modal */}
+            <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
         </View>
     );
 };
