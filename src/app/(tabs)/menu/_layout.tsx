4e6535c0df6641a474400c53e802eb23b9f0d48f
Devin Hunter <devhunter16@gmail.com>
2024-06-26 16:32:53 -0700

    Removing the headerRight code from one _layout.tsx file and putting it into another.

--- src/app/(tabs)/menu/_layout.tsx
+++ src/app/(tabs)/menu/_layout.tsx
@@ -2,7 +2,10 @@
 each other, and you can navigate forward to new screens or backward to previous 
 screens, mimicking the navigation stack behavior commonly found in mobile 
 applications. */
-import { Stack } from 'expo-router';
+import Colors from '@constants/Colors';
+import { FontAwesome } from '@expo/vector-icons';
+import { Link, Stack } from 'expo-router';
+import { Pressable } from 'react-native';
 
 // A _layout.tsx component file wraps around the child components in the same folder.
 // It is also a good place to use data or context providers etc.
@@ -10,6 +13,8 @@ import { Stack } from 'expo-router';
 /* Stack Navigator: Manages a stack of screens, allowing users to navigate between 
 them. Each screen is pushed onto the stack when navigated to, and popped off when 
 navigating back.
+
+Our MenuStack function defines a stack navigator using the Stack component.
 
 Stack.Screen Component: Represents a single screen within the stack navigator. Each
 Stack.Screen component specifies a route and a component that should be rendered 
@@ -20,7 +25,23 @@ for that route. */
 // our app.
 export default function MenuStack() {
     return (
-        <Stack>
+        <Stack
+            screenOptions={{
+                headerRight: () => (
+                    <Link href="/modal" asChild>
+                        <Pressable>
+                            {({ pressed }) => (
+                                <FontAwesome
+                                    name="info-circle"
+                                    size={25}
+                                    color={Colors.light.tint}
+                                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                                />
+                            )}
+                        </Pressable>
+                    </Link>)
+            }}
+        >
             <Stack.Screen name="index" options={{ title: 'Menu' }} />
         </Stack>
     );
