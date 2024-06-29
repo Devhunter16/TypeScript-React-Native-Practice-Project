1c1b291857f583fa0aa989151792d73a425215ce
Devin Hunter <devhunter16@gmail.com>
2024-06-29 16:57:59 -0700

    Beefing up our CartProvider component and using it in other components.

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { Platform, View, Text } from 'react-native';
+import { useContext } from 'react';
+import { CartContext } from '@providers/CartProvider';
 
 const CartScreen = () => {
+    const { items } = useContext(CartContext);
+
     return (
         <View>
-            <Text>Cart</Text>
+            <Text>Cart items length: {items.length}</Text>
 
             {/* Use a light status bar on iOS to account for the black space above the modal */}
             <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
--- src/providers/CartProvider.tsx
+++ src/providers/CartProvider.tsx
@@ -1,10 +1,37 @@
-import { createContext, useContext } from 'react';
+import { Product, CartItem } from '@assets/types/types';
+import { PropsWithChildren, createContext, useState } from 'react';
 
-export const CartContext = createContext({});
+type CartType = {
+    items: CartItem[],
+    addItem: (product: Product, size: CartItem['size']) => void;
+};
 
-const CartProvider = ({ children }) => {
+export const CartContext = createContext<CartType>({
+    items: [],
+    addItem: () => { }
+});
+
+// Importing the PropsWithChildren type from react because TypeScript was yelling at
+// us that the children prop was untyped. 
+const CartProvider = ({ children }: PropsWithChildren<{}>) => {
+    const [items, setItems] = useState<CartItem[]>([]);
+
+    function addItem(product: Product, size: CartItem['size']) {
+        const newCartItem: CartItem = {
+            id: '1',
+            product,
+            product_id: product.id,
+            size,
+            quantity: 1
+        };
+
+        // Settings "items" to an array of the newCartItem and then the rest of the
+        // items that already exist in the cart.
+        setItems([newCartItem, ...items]);
+    };
+
     {/* The value we pass to our CartContext.Provider becomes available to our consumer */ }
-    <CartContext.Provider value={{ items: [], onAddItem: () => { } }}>
+    <CartContext.Provider value={{ items: items, addItem: addItem }}>
         {children}
     </CartContext.Provider>
 };
